3d84e67722e6fcfad28ecbeeed961200f80af91f
davidcrab <davidcrabtree18@icloud.com>
2023-02-09 23:06:47 -0500

    updated marketing

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -42,12 +42,11 @@ const Landing = () => {
           </Center>
         </Box>
         <Box margin="30" mt="150">
-          <Heading size="xl">How?</Heading>
+          <Heading size="xl">Without leaving Sipp</Heading>
           <VStack align="start" ml="5">
-            <Heading size="lg">1. Select specific products from your suppliers</Heading>
-            <Heading size="lg">2. Generate a deck with those product names, descriptions, and images</Heading>
-            <Heading size="lg">3. Add your pricing</Heading>
-            <Heading size="lg">4. Share with your client</Heading>
+            <Heading size="lg">1. Handpick products for your client from your suppliers</Heading>
+            <Heading size="lg">2. Generate a deck with the product names, descriptions, and images</Heading>
+            <Heading size="lg">3. Share with your client</Heading>
           </VStack>
         </Box>
       </Box>
