f01f8471d638d98a38b2ad20553f5f3077cdc5fc
davidcrab <davidcrabtree18@icloud.com>
2023-05-01 22:12:35 -0400

    added playground

--- src/Pages/PlaygroundPage.js
+++ src/Pages/PlaygroundPage.js
@@ -8,96 +8,39 @@ import {
 } from 'reactfire';
 import DeckEditorHeader from '../components/DeckEditor/DeckEditorHeader';
 import DeckEditorProductDisplay from '../components/DeckEditor/DeckEditorProductDisplay';
-import { VStack, Spinner, Heading, Button } from '@chakra-ui/react';
+import { VStack, Spinner, Heading, Button, Image } from '@chakra-ui/react';
 import { useTour } from '@reactour/tour';
 import { InfoOutlineIcon } from '@chakra-ui/icons';
 import EditContactCard from "../components/EditContactCard";
 import { useParams } from 'react-router-dom';
+import deck1 from "./deck1.png";
+import deck2 from "./deck2.png";
+import deck3 from "./deck3.png";
+import deck4 from "./deck4.png";
+import deck5 from "./deck5.png";
+import deck6 from "./deck6.png";
 
 const PlaygroundPage = () => {
-  const { id: deckId } = useParams();
-  const deckRef = doc(useFirestore(), 'decks', deckId);
-  const { setIsOpen } = useTour();
-
-  // subscribe to a document for realtime updates. just one line!
-  const { status: deckStatus, data: deck } = useFirestoreDocData(deckRef);
-
-  let productsQuery = query(
-    collection(useFirestore(), 'showcaseProduct'),
-    where('deckId', '==', deckId)
-  );
-  const { status: productsStatus, data: products } = useFirestoreCollectionData(
-    productsQuery,
-    { idField: 'id' }
-  );
-
-  const handleDeckUpdate = async updatedDeck => {
-    try {
-      await updateDoc(deckRef, updatedDeck);
-    } catch (error) {
-      console.error('Error updating deck:', error);
-    }
-  };
-
-  const handleClientLogoUpload = async event => {
-    const file = event.target.files[0];
-    if (!file) return;
-
-    // Upload the image to Firebase Storage
-    const storage = getStorage();
-    const storageRef = ref(storage, `clientLogos/${file.name}`);
-    await uploadBytes(storageRef, file);
-
-    // Get the uploaded image URL
-    const imageURL = await getDownloadURL(storageRef);
-
-    // Update the Firestore field with the new client logo URL
-    handleDeckUpdate({ ...deck, clientLogo: imageURL });
-  };
-
-  if (deckStatus === 'loading' || productsStatus === 'loading') {
-    return (
-      <VStack>
-        <Spinner size="xl" />
-        <Heading>Fetching products...</Heading>
-      </VStack>
-    );
-  }
-
-  // filter out products with an inactive status
-  const activeProducts = products?.filter(
-    product => product.status !== 'inactive'
-  );
-
   return (
     <div>
-      {productsStatus === 'loading' && <div>Loading...</div>}
-      {deckStatus === 'loading' && <div>Loading...</div>}
-      {productsStatus === 'error' && <div>Error</div>}
-      {deckStatus === 'error' && <div>Error</div>}
-      <DeckEditorHeader
-        deck={deck}
-        onSubmit={handleDeckUpdate}
-        onClientLogoUpload={handleClientLogoUpload}
-      />
-      <EditContactCard deckId={deckId} props={deck.userId} personalNote={deck.personalNote} mb="20"/>
-      {activeProducts &&
-        activeProducts.map(product => {
-          return (
-            <DeckEditorProductDisplay key={product.id} product={product} />
-          );
-        })}
-      <Button
+    <Button
         position="fixed"
-        bottom="5rem"
+        bottom="2rem"
         right="2rem"
         zIndex="99"
-        onClick={() => setIsOpen(true)}
-        leftIcon={<InfoOutlineIcon />}
+        as='a'
+        href={`/`}
+        target="_blank"
+        colorScheme="blue"
       >
-        Open Tour
+        Try it now
       </Button>
-      <FloatingAddButton deckId={deckId} />
+      <Image src={deck1} />
+      <Image src={deck2}  />
+      <Image src={deck3}  />
+      <Image src={deck4}  />
+      <Image src={deck5}  />
+      <Image src={deck6}  />
     </div>
   );
 };
